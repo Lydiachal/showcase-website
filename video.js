e8898b676595b0f64ca5324c739f9287c8405f16
Lydiachal <165217899+Lydiachal@users.noreply.github.com>
2025-02-28 14:12:47 +0100

    Delete video.js

--- video.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    const video = document.querySelector("video");
-    if (video) {
-        video.play().catch(error => console.error("Autoplay failed:", error));
-    }
-};
